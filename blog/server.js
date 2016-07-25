57e3f1978f7ff9719040ac6a56adff9fa4466cb0
Ronaldo <ronaldofjc@hotmail.com>
2016-07-24 22:47:04 -0300

    Projeto de um blog com vue.js express e mongodb - livro Vue.js

--- blog/server.js
+++ blog/server.js
@@ -30,5 +30,61 @@ var router = express.Router();
 //configuração do diretório / como estático, ou seja, todo conteúdo neste diretório
 //será tratado como um arquivo, que quando requisitado, deverá ser entregue ao requisitante
 app.use('/', express.static(__dirname+'/'));
+//middleware: run in all requests
+//código será executado em toda requisição que o express receber
+router.use(function(req, res, next){
+  console.warn(req.method + " " + req.url + " with " + JSON.stringify(req.body));
+  next();
+});
+//middleware:  auth
+//serve para verificar se o token fornecido pela requisição é válido
+var auth = function(req, res, next){
+  //var token recebe o conteúdo do token vindo do cliente
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  if(token){
+    //método jwt.verify serve para analisar o token fornecido pelo cliente
+    jwt.verify(token, secretKey, function(err, decoded){
+      //se o token é inválido
+      if(err){
+        return res.status(403).send({
+          success: false,
+          message: 'Access denied'
+        });
+      //se o token é válido
+      } else {
+        req.decoded = decoded;
+        next(); //método next continua o fluxo da execução
+      }
+    });
+  //se não houver um token sendo repassado pelo cliente
+  } else {
+    return res.status(403).send({
+      success: false,
+      message: 'Access denied'
+    });
+  }
+}
+//simple GET / test
+router.get('/', function(req, res){
+  res.json({message: 'hello world!'});
+});
+//roteamento dos usuários, que será acessado inicialmente pela url /users
+router.route('/users').get(auth, function(req, res){
+  User.find(function(err, users){
+    if(err)
+      res.send(err);
+    res.json(users);
+  });
+}).post(function(req, res){ //método post /users para cadastrar o usuário
+  var user = new User();
+  user.name = req.body.name;
+  user.login = req.body.login;
+  user.password = req.body.password;
+  user.save(function(err){ //salva o registro no banco
+    if(err)
+      res.send(err);
+    res.json(user); //retorna o objeto user ao cliente
+  })
+});
 
-//Parei na página 110 do livro
+//parei na página 114
